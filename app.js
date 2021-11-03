45b4e61ff7c49ef1957ace1894fe346ca1ce905f
Yacouba Keita <theyacoubakeita@gmail.com>
2021-11-03 15:00:01 +0000

    Final commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const icon = document.getElementById('icon');
+const password = document.getElementById('password')
+
+icon.addEventListener('click', function() {
+    if(password.type == 'password'){
+        password.type = 'text'
+        icon.classList.add("fa-eye-slash");
+
+    } else {
+        password.type = 'password'
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+
+    }
+});
+
+
