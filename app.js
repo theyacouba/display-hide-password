fffd39840fa3ac6b68ca49bf5b1f8cb9564e3816
Yacouba Keita <theyacoubakeita@gmail.com>
2021-11-03 15:23:19 +0000

    Fixes

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ icon.addEventListener('click', function() {
     if(password.type == 'password'){
         password.type = 'text'
         icon.classList.add("fa-eye-slash");
-
     } else {
         password.type = 'password'
         icon.classList.remove("fa-eye-slash");
         icon.classList.add("fa-eye");
-
     }
 });
-
-
